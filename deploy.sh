b766164ef045103b627cfc150c7274016781597e
MichaelGit202 <95455862+MichaelGit202@users.noreply.github.com>
2025-04-12 16:04:07 -0400

    deployscripts

--- deploy.sh
+++ deploy.sh
@@ -19,10 +19,10 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:MichaelGit202/MichaelGit202.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:MichaelGit202/hackathon-showcase-gallery.git main:gh-pages
 
 cd -
 
